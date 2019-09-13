9f8ca4497ad66ab0b44700fc1b91bf2a41953222
skai <skaidevs@gmail.com>
2019-09-13 08:59:56 -0500

    horizontal ListView 50%

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+//My imports
+import 'package:e_commerce/componets/horizontal_listview.dart';
 
 void main() => runApp(MyApp());
 
@@ -21,8 +24,29 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    Widget imageCarousel = new Container(
+      height: 200.0,
+      child: Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          AssetImage('images/w.jpg'),
+          AssetImage('images/c1.jpg'),
+          AssetImage('images/w1.jpg'),
+          AssetImage('images/w3.jpg'),
+          AssetImage('images/m2.jpg'),
+          AssetImage('images/m1.jpg'),
+        ],
+        autoplay: true,
+        animationCurve: Curves.fastLinearToSlowEaseIn,
+        animationDuration: Duration(milliseconds: 1000),
+        dotSize: 4.0,
+        indicatorBgPadding: 8.0,
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
+        elevation: 0.0,
         backgroundColor: Colors.black,
         title: Text('Store'),
         actions: <Widget>[
@@ -59,10 +83,11 @@ class _MyHomePageState extends State<MyHomePage> {
               decoration: BoxDecoration(color: Colors.black),
             ),
             //Body
+            // This is the body of the home page
             InkWell(
               onTap: () {},
               child: ListTile(
-                title: Text('Home Page'),
+                title: Text('Home Page '),
                 leading: Icon(Icons.home),
               ),
             ),
@@ -118,6 +143,73 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          imageCarousel,
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              'Categories',
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18.0),
+            ),
+            //Horizontal List View
+          ),
+          Container(
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'This is the main Body',
+                  style: TextStyle(fontSize: 50.0),
+                ),
+                Text(
+                  'This is the main Body',
+                  style: TextStyle(fontSize: 50.0),
+                ),
+                Text(
+                  'This is the main Body',
+                  style: TextStyle(fontSize: 50.0),
+                ),
+                Text(
+                  'This is the main Body',
+                  style: TextStyle(fontSize: 50.0),
+                ),
+                Text(
+                  'This is the main Body',
+                  style: TextStyle(fontSize: 50.0),
+                ),
+                Text(
+                  'This is the main Body',
+                  style: TextStyle(fontSize: 50.0),
+                ),
+                Text(
+                  'This is the main Body',
+                  style: TextStyle(fontSize: 50.0),
+                ),
+                Text(
+                  'This is the main Body',
+                  style: TextStyle(fontSize: 50.0),
+                ),
+                Text(
+                  'This is the main Body',
+                  style: TextStyle(fontSize: 50.0),
+                ),
+                Text(
+                  'This is the main Body',
+                  style: TextStyle(fontSize: 50.0),
+                ),
+                Text(
+                  'This is the main Body',
+                  style: TextStyle(fontSize: 50.0),
+                ),
+                Text(
+                  'This is the main Body',
+                  style: TextStyle(fontSize: 50.0),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
